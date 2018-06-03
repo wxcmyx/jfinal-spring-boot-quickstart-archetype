2b8e731d6a531cbc99fc785d36776444fdd9641d
xiaochaoWang <wxcmyx@163.com>
2018-06-03 17:20:31 +0800

    加入redis,mongodb,logback等

---
+++ target/classes/archetype-resources/src/main/java/__packageInPathFormat__/web/controller/ErrorController.java
@@ -0,0 +1,25 @@
+package ${package}.web.controller;
+
+import ${package}.web.rest.Result;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+@ApiIgnore
+@RestController
+@EnableAutoConfiguration
+public class ErrorController {
+
+    @RequestMapping("/404")
+    @ResponseBody
+    public Result error404() {
+        return new Result().set404();
+    }
+    @RequestMapping("/500")
+    @ResponseBody
+    public Result error500() {
+        return new Result().set500();
+    }
+}
